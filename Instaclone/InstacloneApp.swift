3f07202c8b898daa9188f0bd0dc7ffa9c27218af
Konstantin Tarasenko <t.constantin@gmail.com>
2023-08-06 16:41:06 +0500

    Initial Commit

---
+++ Instaclone/InstacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstacloneApp.swift
+//  Instaclone
+//
+//  Created by Konstantin Tarasenko on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct InstacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
